31ceea6b68706d5684a860f71738fd445dfdbcb0
khmdagal <khmdagal@gmail.com>
2022-11-13 14:54:18 +0000

    Change HTTP -TO- HTTPS to unblock the request

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ let facts = document.querySelector("#fact");
 let factsText = document.querySelector("#factText");
 
 let input = document.querySelector("#numberInput");
-input.addEventListener("input",getFactAjax);
+input.addEventListener("input", getFactAjax);
 
 //getFactFetch
 function getFactAjax() {
   let number = input.value;
 
   let xhr = new XMLHttpRequest();
-  xhr.open("GET", `http://numbersapi.com/${number}`);
-    xhr.onload = function () {
-      // http 200 status  means every thing is okay
-        
-        if (this.status == 200 && number != "") {
-            facts.style.display = "block"
-            factsText.innerText = this.responseText;
-            console.log(this.responseText)
-        }
+  xhr.open("GET", `https://numbersapi.com/${number}`);
+  xhr.onload = function () {
+    // http 200 status  means every thing is okay
+
+    if (this.status == 200 && number != "") {
+      facts.style.display = "block";
+      factsText.innerText = this.responseText;
+      console.log(this.responseText);
     }
-    
-    xhr.send();
+  };
+
+  xhr.send();
 }
 
 /*
